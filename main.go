b515509fb78d031f12ffa199c409f94e128c1d46
rikyuusima <equanz324@gmail.com>
2017-12-01 14:49:26 +0900

    add slack bot notificator

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 // import
 import(
   "github.com/gin-gonic/gin"
+  "os"
+  "github.com/nlopes/slack"
 )
 
 // server
@@ -12,6 +14,7 @@ func main(){
   // root
   r.GET("/", func(c *gin.Context){
     c.String(200, "Hello, world!")
+    send_form_message(true)
   })
 
   // key on
@@ -20,5 +23,21 @@ func main(){
   })
 
   r.Run()
+}
+
+func send_form_message(is_onboard bool){
+  // slack bot api instance
+  api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+  // slack rtm instance
+  rtm := api.NewRTM()
+  go rtm.ManageConnection()
+
+  if is_onboard == true {
+    // on board
+    rtm.SendMessage(rtm.NewOutgoingMessage("ON Board!", os.Getenv("CHANNEL")))
+  } else {
+    // out board
+    rtm.SendMessage(rtm.NewOutgoingMessage("OUT Board!", os.Getenv("CHANNEL")))
+  }
 }
 
