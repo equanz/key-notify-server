20cc5b84c79f786c8876607cd6116a14eb08aaf2
rikyuusima <equanz324@gmail.com>
2017-12-16 14:03:51 +0900

    add statistics api

--- main.go
+++ main.go
@@ -21,7 +21,9 @@ func main(){
   // '/api' group
   api := r.Group("/api")
   {
-    // key on or off
+    /* key on or off
+     * :status: string "on", "off"文字列をそれぞれハードウェアのon, offとして処理
+    */
     api.POST("/hard/:status", func(c *gin.Context){
       state := c.Param("status")
       if state == "on"{
@@ -33,6 +35,21 @@ func main(){
       } else{
         // error
         c.String(400, "Bad Request")
+      }
+    })
+
+    /* on/off統計データ(rawデータ)を指定日時から最新まで返す
+     * first_date: string(DATETIMEフォーマット) 取得する統計値の指定日時
+    */
+    api.GET("/statistics", func(c *gin.Context){
+      q := c.Request.URL.Query() // query params
+      fds, fd_ok := q["first_date"]
+
+      // TODO: return result about statistics data from SQL
+      if fd_ok == true {
+        c.String(200, fds[0])
+      } else{
+        c.String(200, "ALL")
       }
     })
   }
