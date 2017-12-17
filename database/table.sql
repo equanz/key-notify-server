2d50f2d01d08949f0583736b39cda3d788bb9781
amataku <amataku13@gmail.com>
2017-12-18 00:53:46 +0900

    add comment

--- database/table.sql
+++ database/table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE key_info(
-  time DATETIME NOT NULL,
-  state ENUM('ON','OFF') NOT NULL,
-  key_info_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+  time DATETIME NOT NULL, --アクション時刻
+  state ENUM('ON','OFF') NOT NULL, --鍵の状態
+  key_info_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY --ID
 );
