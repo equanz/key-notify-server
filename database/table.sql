e781aaa70ec461697797f75a05ace6c298f24e9b
equanz <equanz324@gmail.com>
2018-02-07 11:26:48 +0900

    feat: create new table(app_id)

--- database/table.sql
+++ database/table.sql
@@ -3,3 +3,9 @@ CREATE TABLE key_info(
   state ENUM('ON','OFF') NOT NULL, --鍵の状態
   key_info_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY --ID
 );
+
+CREATE TABLE app_id(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, -- ID
+  app_id VARCHAR(48) NOT NULL -- app_id
+);
+
