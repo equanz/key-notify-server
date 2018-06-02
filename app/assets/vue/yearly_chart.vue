8f2024df5c9831b8b69dd887880f3d363117fd2e
equanz <equanz324@gmail.com>
2018-06-02 18:16:56 +0900

    feat: add yearly label

--- app/assets/vue/yearly_chart.vue
+++ app/assets/vue/yearly_chart.vue
@@ -6,7 +6,7 @@
     props: ['raw_data', 'fiscal_year'],
     mounted() {
       this.renderChart({
-        labels: Array(52).fill(0),
+        labels: createYearLabel(this.fiscal_year),
         datasets: compilePlotData(this.raw_data, this.fiscal_year),
       }, { // options
         tooltips: {
@@ -27,45 +27,50 @@
    */
   let compilePlotData = (raw_data, fiscal_year) => {
     // property
-    let plot_data = [] //initialize
+    let plot_data = Array(getWeekVal(fiscal_year)).fill(0) //initialize
     let raw_data_index = 0 // initialize
     let index_date = new Date(fiscal_year, 3, 1) // initialize to 4/1
 
     // const
     const BACK_COLOR =  '#f87979'
 
-    // allocate plot_data
-    // leap year
-    if(isLeapYear(fiscal_year) == true){
-      // if last day by fiscal year is sunday, to allocate 53
-      const LAST_DAY_FISCAL_YEAR = new Date(fiscal_year + 1, 2, 31)
-
-      // sunday
-      if(LAST_DAY_FISCAL_YEAR.getDay() == 0){
-        plot_data = Array(53).fill(0)
-      } else{ // not sunday
-        plot_data = Array(52).fill(0)
-      }
-
-    } else{ // normal year
-      plot_data = Array(52).fill(0)
-    }
+    // copy array
+    raw_data = raw_data.concat()
 
     // set index_date to first sunday
     index_date.setDate(index_date.getDate() - index_date.getDay())
 
+
     for(let i = 0; i < plot_data.length; i++){
       let plot_value = 0
 
-      while(raw_data_index < raw_data.length - 1){
+      while(raw_data_index < raw_data.length){
         let time_date = new Date(raw_data[raw_data_index].Time)
         // state is ON
         if(raw_data[raw_data_index].State == "ON"){
-          console.log('hoge')
-          console.log(time_date)
-          console.log(index_date)
           if(isSameWeek(time_date, index_date) == true){
-            let off_time_date = new Date(raw_data[raw_data_index + 1].Time)
+            let off_time_date = {} // initialize
+            if(raw_data_index < raw_data.length - 1){
+              off_time_date = new Date(raw_data[raw_data_index + 1].Time)
+            } else{
+              let now_time_date = new Date()
+              let now_fiscal_year = 0
+
+              // calc fiscal year
+              if(now_time_date.getMonth() < 3){
+                now_fiscal_year = now_time_date.getFullYear() + 1
+              } else{
+                now_fiscal_year = now_time_date.getFullYear()
+              }
+              // now year
+              if(time_date.getFullYear() == now_fiscal_year){
+                off_time_date = new Date()
+              } else{ // not now year
+                off_time_date = new Date(time_date.getFullYear(), 2, 31, 23, 59, 59, 999) // 3/31 23:59:59;999
+                raw_data.push(off_time_date)
+              }
+            }
+
             if(isSameWeek(off_time_date, index_date) == true){
               // add value
               plot_value += getDiffHour(time_date, off_time_date)
@@ -109,17 +114,7 @@
       }
 
       plot_data[i] = plot_value
-      // insert to plot_data
-      /* plot_data[i] = {
-       *   backgroundColor: BACK_COLOR,
-       *   data: [plot_value]
-       * } */
     }
-
-    console.log([{
-      backgroundColor: BACK_COLOR,
-      data: plot_data
-    }])
 
     return [{
       backgroundColor: BACK_COLOR,
@@ -181,5 +176,50 @@
     diff_hour = Math.floor(diff_millsec / (1000 * 60 * 60))
 
     return diff_hour
+  }
+
+  /**
+   * if last day by fiscal year is sunday, return 53, otherwise return 52
+   * @param year {Number} - fiscal year
+   * @return {Number} return week value
+   */
+  let getWeekVal = (year) => {
+    // const
+    const LEAP_WEEK = 53
+    const NORMAL_WEEK = 53
+    const LAST_DAY_FISCAL_YEAR = new Date(year + 1, 2, 31)
+    if(isLeapYear(year) == true){
+      // sunday
+      if(LAST_DAY_FISCAL_YEAR.getDay() == 0){
+        return LEAP_WEEK
+      } else{ // not sunday
+        return NORMAL_WEEK
+      }
+
+    } else{ // normal year
+      return NORMAL_WEEK
+    }
+  }
+
+  let createYearLabel = (year) => {
+    // property
+    let index_date = new Date(year, 3, 1) // initialize to 4/1
+    let year_label = Array(getWeekVal(year)).fill("")
+    let before_month = -1
+
+    // set index_date to first sunday
+    index_date.setDate(index_date.getDate() - index_date.getDay())
+
+    for(let i = 0; i < year_label.length; i++){
+      if(index_date.getMonth() != before_month){
+        year_label[i] = `${index_date.getMonth() + 1}`
+        before_month = index_date.getMonth()
+      }
+
+      // increment
+      index_date.setDate(index_date.getDate() +7)
+    }
+
+    return year_label
   }
 </script>
