55d299336ae019bd910e7c8531b725d999598118
amataku <amataku13@gmail.com>
2018-06-03 01:59:34 +0900

    add:same access proced

--- main.go
+++ main.go
@@ -41,26 +41,56 @@ func main(){
       state := c.Param("status")
 
       has_app_id, err := sql_query.Has_app_id(body.AppID) // search app_id
+      before_array, err_array := sql_query.Get_latest_state() //get bsfore state
       if err == nil {
-        if body.AppID != "" && has_app_id == true {
-          if state == "on" {
-            if send_form_message(true) == nil {
-              if sql_query.Insert_status("ON") == nil {
-                c.String(200, "ON")
+        if err_array == nil {
+          if body.AppID != "" && has_app_id == true {
+            if state == "on" {
+              if send_form_message(true) == nil {
+                if before_array[0].State != "ON" {
+                  if sql_query.Insert_status("ON") == nil {
+                    c.String(200, "ON")
+                  } else {
+                    c.String(400, "Bad Request")
+                  }
+                } else {
+                  if sql_query.Insert_status("OFF") == nil {
+                    if sql_query.Insert_status("ON") == nil {
+                      c.String(200, "ON")
+                    } else {
+                      c.String(400, "Bad Request")
+                    }
+                  } else {
+                    c.String(400, "Bad Request")
+                  }
+                }
+              } else {
+                c.String(400, "Bad Request")
+              }
+            } else if state == "off" {
+              if send_form_message(false) == nil {
+                if before_array[0].State != "OFF" {
+                  if sql_query.Insert_status("OFF") == nil {
+                    c.String(200, "OFF")
+                  } else {
+                    c.String(400, "Bad Request")
+                  }
+                } else {
+                  if sql_query.Insert_status("ON") == nil {
+                    if sql_query.Insert_status("OFF") == nil {
+                      c.String(200, "OFF")
+                    } else {
+                      c.String(400, "Bad Request")
+                    }
+                  } else {
+                    c.String(400, "Bad Request")
+                  }
+                }
               } else {
                 c.String(400, "Bad Request")
               }
             } else {
-              c.String(400, "Bad Request")
-            }
-          } else if state == "off" {
-            if send_form_message(false) == nil {
-              if sql_query.Insert_status("OFF") == nil {
-                c.String(200, "OFF")
-              } else {
-                c.String(400, "Bad Request")
-              }
-            } else {
+              // error
               c.String(400, "Bad Request")
             }
           } else {
@@ -68,8 +98,7 @@ func main(){
             c.String(400, "Bad Request")
           }
         } else {
-          // error
-          c.String(400, "Bad Request")
+          c.String(503, "Service Unavailable")
         }
       } else {
         // error
